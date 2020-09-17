9ad71bfa89f3abb4519d31064b41052398887bc1
Shai Bennathan <shai.bennathan@intel.com>
2020-09-17 16:01:57 +0300

    prevent navigating away

--- src/build/RobotBuilder.vue
+++ src/build/RobotBuilder.vue
@@ -76,10 +76,21 @@ import CollapsibleSection from '../shared/CollapsibleSection.vue';
 
 export default {
   name: 'RobotBuilder',
+  beforeRouteLeave(to, from, next) {
+    if (this.addedToCard) {
+      next(true);
+    } else {
+      /* eslint no-alert: 0 */
+      /* eslint no-restricted-globals: 0 */
+      const response = confirm('You have not added your robot to your cart, are you sure you want to leave?');
+      next(response);
+    }
+  },
   components: { PartSelector, CollapsibleSection },
   data() {
     return {
       availableParts,
+      addedToCard: false,
       cart: [],
       selectedRobot: {
         head: {},
@@ -112,6 +123,7 @@ export default {
        + robot.rightArm.cost
        + robot.base.cost;
       this.cart.push({ ...robot, cost });
+      this.addedToCard = true;
     },
   },
 };
