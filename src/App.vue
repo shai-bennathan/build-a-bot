815948e88284aa30b6d2f18e9d27607803baafab
shaib-intel <shai.bennathan@intel.com>
2020-09-23 17:03:58 +0300

    mapActions

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,11 @@
 <template>
   <div id="app">
-    Root Foo: {{rootFoo}} <br/>
+    <!-- Root Foo: {{rootFoo}} <br/>
     Robots Foo: {{robotsFoo}} <br/>
     Users Foo: {{usersFoo}} <br/>
     <br/>
     Root Getter Foo: {{rootGetterFoo}} <br/>
-    Robots Getter Foo: {{robotsGetterFoo}} <br/>
+    Robots Getter Foo: {{robotsGetterFoo}} <br/> -->
     <header>
       <nav>
         <ul>
--- src/build/RobotBuilder.vue
+++ src/build/RobotBuilder.vue
@@ -52,6 +52,7 @@
 </template>
 
 <script>
+import { mapActions } from 'vuex';
 import createdHookMixin from './created-hook-mixin';
 import PartSelector from './PartSelector.vue';
 import CollapsibleSection from '../shared/CollapsibleSection.vue';
@@ -59,7 +60,7 @@ import CollapsibleSection from '../shared/CollapsibleSection.vue';
 export default {
   name: 'RobotBuilder',
   created() {
-    this.$store.dispatch('robots/getParts');
+    this.getParts();
   },
   beforeRouteLeave(to, from, next) {
     if (this.addedToCard) {
@@ -102,6 +103,7 @@ export default {
     },
   },
   methods: {
+    ...mapActions('robots', ['getParts', 'addRobotToCart']),
     addToCart() {
       const robot = this.selectedRobot;
       const cost = robot.head.cost
@@ -109,7 +111,7 @@ export default {
        + robot.torso.cost
        + robot.rightArm.cost
        + robot.base.cost;
-      this.$store.dispatch('robots/addRobotToCart', { ...robot, cost })
+      this.addRobotToCart({ ...robot, cost })
         .then(() => this.$router.push('/cart'));
       this.addedToCard = true;
     },
