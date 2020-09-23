f2ac9622a82ec145350be4dccdd8777153dc1afb
shaib-intel <shai.bennathan@intel.com>
2020-09-23 16:59:32 +0300

    mapGetters

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,6 @@
     <br/>
     Root Getter Foo: {{rootGetterFoo}} <br/>
     Robots Getter Foo: {{robotsGetterFoo}} <br/>
-    Users Getter Foo: {{usersGetterFoo}} <br/>
     <header>
       <nav>
         <ul>
@@ -44,7 +43,7 @@
 </template>
 
 <script>
-import { mapState } from 'vuex';
+import { mapState, mapGetters } from 'vuex';
 
 export default {
   name: 'App',
@@ -54,15 +53,8 @@ export default {
       usersFoo: (state) => state.users.foo,
     }),
     ...mapState('robots', { robotsFoo: 'foo' }),
-    rootGetterFoo() {
-      return this.$store.getters.foo;
-    },
-    robotsGetterFoo() {
-      return this.$store.getters['robots/foo'];
-    },
-    usersGetterFoo() {
-      return this.$store.getters['users/foo'];
-    },
+    ...mapGetters({ rootGetterFoo: 'foo' }),
+    ...mapGetters('robots', { robotsGetterFoo: 'foo' }),
     cart() {
       return this.$store.state.robots.cart;
     },
